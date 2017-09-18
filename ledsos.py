d8915d87e6cb6ad6e85918bf23b41cb74349e6b1
chewbacca340 <braydentferrell@gmail.com>
2017-09-18 18:23:02 +0000

    made the led flash once

--- ledsos.py
+++ ledsos.py
@@ -1,1 +1,12 @@
-£test
+import RPi.GPIO as gpio
+#led is on 18
+import time
+gpio.setmode(gpio.BCM)
+gpio.setwarnings(False)
+gpio.setup(18,gpio.OUT)
+
+gpio.output(18,1)
+print "led on"
+time.sleep(3)
+gpio.output(18,0)
+print "led off"
