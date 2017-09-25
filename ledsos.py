581909a7b792736f56fe5781dfac2d5d7d6a328c
chewbacca340 <braydentferrell@gmail.com>
2017-09-25 01:43:41 +0000

    sos fixed

--- ledsos.py
+++ ledsos.py
@@ -5,8 +5,21 @@ gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
 gpio.setup(18,gpio.OUT)
 
-gpio.output(18,1)
-print "led on"
-time.sleep(3)
-gpio.output(18,0)
-print "led off"
+for x in range(0, 3):
+  gpio.output(18,1)
+  #print "led on"
+  time.sleep(1)
+  gpio.output(18,0)
+  #print "led off"
+  time.sleep(1)
+
+for x in range(0, 3):
+  gpio.output(18,1)
+  time.sleep(3)
+  gpio.output(18,0)
+  time.sleep(1)
+
+for x in range(0,3):
+  gpio.output(18,1)
+  time.sleep(1)
+  gpio.output(18,0)
